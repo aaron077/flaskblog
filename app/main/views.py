ff1793c535c7bf39eee5f5e6bcbf209538007034
aaron077 <wenmiaodeng@gmail.com>
2016-05-03 16:24:01 +0800

    article detail initial version

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template, request, current_app, redirect,\
     url_for, flash
 from . import main
 from ..models import Article, ArticleType, \
-    Source,BlogView
+    Source,BlogView,Comment
 from .. import db
 
 @main.route('/')
@@ -41,3 +41,28 @@ def article_sources(id):
     return render_template('index.html', articles=articles,
                            pagination=pagination, endpoint='.article_sources',
                            id=id)
+
+@main.route('/article-details/<int:id>',methods=['GET','POST'])
+def articleDetails(id):
+    BlogView.add_view(db)
+
+    article = Article.query.get_or_404(id)
+    print("111")
+    page = request.args.get('page', 1, type=int)
+    if page == -1:
+        page = (article.comments.count() - 1) // \
+            current_app.config['COMMENTS_PER_PAGE'] + 1
+    print("aaa %s",article.comments.count())
+    pagination = article.comments.order_by(Comment.timestamp.asc()).paginate(
+        page, per_page=current_app.config['COMMENTS_PER_PAGE'],
+        error_out=False)
+    print("bbb")
+    comments = pagination.items
+    print("ccc")
+    article.add_view(article, db)
+    print("ddd")
+    return render_template('article_details.html',article=article,
+                           comments=comments, pagination=pagination, page=page,
+                           endpoint='.articleDetails', id=article.id)
+    # page=page, this is used to return the current page args to the
+    # disable comment or enable comment endpoint to pass it to the articleDetails endpoint
